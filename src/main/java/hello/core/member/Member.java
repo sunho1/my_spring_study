e2613ca49835499c9ae2bbd05aa1ed12932f99d0
이선호 <zpfh1638@naver.com>
2022-10-25 17:51:13 +0900

    commit

--- src/main/java/hello/core/member/Member.java
+++ src/main/java/hello/core/member/Member.java
@@ -1,9 +1,20 @@
 package hello.core.member;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Member {
     private Long id;
     private String name;
     private Grade grade;
+
+    public Member(){
+
+    }
 
     public Member(Long id, String name, Grade grade) {
         this.id = id;
@@ -34,4 +45,6 @@ public class Member {
     public void setGrade(Grade grade) {
         this.grade = grade;
     }
+
+
 }
